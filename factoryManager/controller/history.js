7ed344b702dd451064dfa3f0978b3d7b1981857f
pratikwadke02 <82306536+pratikwadke02@users.noreply.github.com>
2024-03-07 02:04:57 +0530

    order history fix

--- factoryManager/controller/history.js
+++ factoryManager/controller/history.js
@@ -13,8 +13,20 @@ const getOrderHistroy = async (req, res) => {
                 message: "Please provide orderId"
             });
         }
-        const orderHistory = await All_Models.OrderHistory_Model.findAll({
-            where: whereClause
+        let orderHistory = await All_Models.OrderHistory_Model.findAll({
+            where: whereClause,
+        });
+        //find all the user from updatedById column
+        const updatedBy = orderHistory.map(order => order.updatedById);
+        const users = await All_Models.User_Model.findAll({
+            where: {
+                id: updatedBy
+            }
+        });
+        //map the user with orderHistory
+        orderHistory.forEach(order => {
+            let user = users.find(user => user.id === order.updatedById);
+            order.dataValues.updatedBy = user;
         });
         res.status(200).json({
             message: "Order History",
